50ff45a6388233c5fda929de12a869052084f2d7
101 <tmddnjs001133@dsm.hs.kr>
2024-04-14 21:17:41 +0900

    add :: UserAlreadyExistsByEmailException.java

---
+++ awack-common/src/main/java/org/awack/custom/UserAlreadyExistsByEmailException.java
@@ -0,0 +1,14 @@
+package org.awack.custom;
+
+import org.awack.error.ErrorCode;
+import org.awack.error.exception.CustomException;
+
+public class UserAlreadyExistsByEmailException extends CustomException {
+    public static final CustomException EXCEPTION
+            = new UserAlreadyExistsByEmailException();
+
+    private UserAlreadyExistsByEmailException() {
+        super(ErrorCode.USER_EXISTS_BY_EMAIL);
+    }
+
+}
