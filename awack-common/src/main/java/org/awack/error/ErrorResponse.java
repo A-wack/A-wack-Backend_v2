e8f190720bda88700e26c4d05fcddc65cd59ad39
101 <tmddnjs001133@dsm.hs.kr>
2024-04-14 21:15:42 +0900

    add :: ErrorResponse.java

---
+++ awack-common/src/main/java/org/awack/error/ErrorResponse.java
@@ -0,0 +1,33 @@
+package org.awack.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+public record ErrorResponse(
+        int status,
+        String message
+) {
+
+    public static ErrorResponse of(int status, String message) {
+        return new ErrorResponse(status, message);
+    }
+
+    public static ResponseEntity<ErrorResponse> of(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(of(errorCode.getStatus(), errorCode.getMessage()));
+    }
+
+    public static ResponseEntity<ErrorResponse> of(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        int status = HttpStatus.BAD_REQUEST.value();
+        String message = fieldError != null ? fieldError.getDefaultMessage() : "Bad Request";
+
+        return ResponseEntity
+                .status(status)
+                .body(ErrorResponse.of(status, message));
+    }
+
+}
