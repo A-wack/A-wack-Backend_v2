e9a58d798dd9561bdf3cef9f829579b47e33a526
101 <tmddnjs001133@dsm.hs.kr>
2024-04-26 15:23:59 +0900

    add :: CertifiedMailRepository.java

---
+++ awack-domain/src/main/java/org/awack/domain/mail/certifiedmail/dao/CertifiedMailRepository.java
@@ -0,0 +1,7 @@
+package org.awack.domain.mail.certifiedmail.dao;
+
+import org.awack.domain.mail.certifiedmail.model.CertifiedMail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CertifiedMailRepository extends CrudRepository<CertifiedMail, String> {
+}
