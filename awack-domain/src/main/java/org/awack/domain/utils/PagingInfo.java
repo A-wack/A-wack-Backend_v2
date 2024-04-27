2730d4a49242692502f5faeffd3e8ffef55e7bd5
101 <tmddnjs001133@dsm.hs.kr>
2024-04-27 21:16:20 +0900

    add :: PagingInfo.java

---
+++ awack-domain/src/main/java/org/awack/domain/utils/PagingInfo.java
@@ -0,0 +1,22 @@
+package org.awack.domain.utils;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+public record PagingInfo(
+        Integer page,
+        Integer size
+) {
+
+    public static PagingInfo of(Integer page, Integer size) {
+        return new PagingInfo(page, size);
+    }
+
+    public Pageable pageable() {
+        return PageRequest.of(
+                page(),
+                size()
+        );
+    }
+
+}
