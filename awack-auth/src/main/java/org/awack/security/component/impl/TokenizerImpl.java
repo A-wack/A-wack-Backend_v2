d491538d43fcd43060d25868d3c586d8e8dd1e2c
101 <tmddnjs001133@dsm.hs.kr>
2024-05-14 08:56:38 +0900

    refactor ::

--- awack-auth/src/main/java/org/awack/security/component/impl/TokenizerImpl.java
+++ awack-auth/src/main/java/org/awack/security/component/impl/TokenizerImpl.java
@@ -1,11 +1,9 @@
 package org.awack.security.component.impl;
 
 import io.jsonwebtoken.*;
-import jakarta.websocket.OnError;
 import lombok.RequiredArgsConstructor;
 import org.awack.custom.JwtExpiredException;
 import org.awack.custom.UserNotFoundException;
-import org.awack.domain.user.model.UserEntity;
 import org.awack.domain.user.port.FindUserPort;
 import org.awack.security.component.Tokenizer;
 import org.awack.security.config.SecurityProperties;
@@ -13,11 +11,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -29,17 +25,17 @@ public class TokenizerImpl implements Tokenizer {
 
     @Override
     public String create(String email) {
-        Date now = new Date();
+        var now = new Date();
 
         return Jwts.builder()
                 .setSubject(email)
-                .signWith(SignatureAlgorithm.HS512, properties.getSecret())
+                .signWith(SignatureAlgorithm.HS512, properties.secret())
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + properties.getExpiration()))
-                .setIssuer(properties.getIssuer())
+                .setExpiration(new Date(now.getTime() + properties.expiration()))
+                .setIssuer(properties.issuer())
                 .setNotBefore(now)
                 .setHeaderParam("typ", "JWT")
-                .setHeaderParam("alg", properties.getAlgorithm())
+                .setHeaderParam("alg", properties.algorithm())
                 .compact();
     }
 
@@ -47,12 +43,12 @@ public class TokenizerImpl implements Tokenizer {
     public boolean validate(String token) {
         return getClaims(token)
                 .getIssuer()
-                .equals(properties.getIssuer());
+                .equals(properties.issuer());
     }
 
     private Claims getClaims(String token) {
         return Jwts.parser()
-                .setSigningKey(properties.getSecret())
+                .setSigningKey(properties.secret())
                 .parseClaimsJws(token)
                 .getBody();
     }
@@ -69,34 +65,34 @@ public class TokenizerImpl implements Tokenizer {
             validate(token);
             return create(getSubject(token));
         } catch (ExpiredJwtException e) {
-            String email = e.getClaims().getSubject();
+            var email = e.getClaims().getSubject();
 
-            Date expiration = e.getClaims().getExpiration();
+            var expiration = e.getClaims().getExpiration();
 
-            Calendar cal = Calendar.getInstance();
+            var cal = Calendar.getInstance();
             cal.setTime(expiration);
             cal.add(Calendar.HOUR_OF_DAY, 2);
 
-            Date twoHoursLater = cal.getTime();
+            var twoHoursLater = cal.getTime();
 
-            Date now = new Date();
+            var now = new Date();
             if (now.before(twoHoursLater)) {
                 return create(email);
             } else {
-                throw JwtExpiredException.EXCEPTION;
+                throw new JwtExpiredException();
             }
         } catch (JwtException e) {
-            throw JwtExpiredException.EXCEPTION;
+            throw new JwtExpiredException();
         }
     }
 
     @Override
     public Authentication getAuthentication(String token) {
-        String subject = getSubject(token);
-        UserEntity user = findUserPort.findByEmail(subject)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        var subject = getSubject(token);
+        var user = findUserPort.findByEmail(subject)
+                .orElseThrow(UserNotFoundException::new);
 
-        UserDetails userDetails = new User(subject, "", List.of(
+        var userDetails = new User(subject, "", List.of(
                 new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
         ));
 
--- awack-auth/src/main/java/org/awack/security/config/SecurityProperties.java
+++ awack-auth/src/main/java/org/awack/security/config/SecurityProperties.java
@@ -1,17 +1,12 @@
 package org.awack.security.config;
 
-import lombok.Data;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Data
-@Component
 @ConfigurationProperties(prefix = "awack.security")
-public class SecurityProperties {
-    private String secret;
-    private String issuer;
-    private long expiration;
-    private String algorithm;
-
+public record SecurityProperties(
+        String secret,
+        String issuer,
+        Long expiration,
+        String algorithm
+) {
 }
